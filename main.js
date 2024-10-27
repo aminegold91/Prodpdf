8f4395a7eeb3f107a58757eec93eae3cc53ac131
aminegold91 <aminegold91@gmail.com>
2024-10-27 16:33:46 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,33 @@
+fetch("data.json")
+.then(function(res) {
+  return res.json();
+})
+.then(function(products) {
+  
+  let bd = document.querySelector('.tb');
+  let out ='';
+  let card =document.querySelector('.card');
+  let four = '';
+  for(let product of products){
+    out += `
+     <tr>
+         
+          <td>
+          <img src='${product.img}'/>
+          
+          </td>
+          <td>${product.name}</td>
+          <td>${product.price}</td>
+          <td>${product.praces}</td>
+          
+      </tr>
+    `;
+  }
+  bd.innerHTML = out;
+})
+
+let btn = document.querySelector('button');
+btn.addEventListener('click',()=>{
+  btn.style.display='none';
+  print();
+})
